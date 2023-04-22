ba458ed73b55eca9cd7c4671f1821d86296ffed5
Mayna Azevedo <70771139+maynahelena@users.noreply.github.com>
2023-04-22 18:43:10 -0300

    Add files via upload

---
+++ script/scriptContato.js
@@ -0,0 +1,10 @@
+const form = document.getElementById("contact-form");
+const messageContainer = document.getElementById("message-container");
+
+form.addEventListener("submit", (e) => {
+  e.preventDefault();
+
+  const name = document.getElementById("name").value;
+
+  const age = document.getElementById("age").value;
+});
